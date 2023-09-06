bfffe143d85ef596e5cfbaf84a548d9f099f3106
Vinithkumar001 <122115335+Vinithkumar001@users.noreply.github.com>
2023-09-06 18:03:27 +0530

    Update matrix.py

--- matrix.py
+++ matrix.py
@@ -1,17 +1,30 @@
-def count_zeros(matrix):
-    count = 0
-    for row in matrix:
-        for element in row:
-            if element == 0:
-                count += 1
-    return count
+name: Count Zeros in Matrix
 
-# Example matrix
-matrix = [
-    [1, 0, 2],
-    [0, 3, 4],
-    [5, 0, 6]
-]
+on:
+  push:
+    branches:
+      - main  # Adjust this branch name as needed
 
-zeros_count = count_zeros(matrix)
-print("Number of zeros in the matrix:", zeros_count)
+jobs:
+  build:
+    runs-on: ubuntu-latest
+
+    steps:
+    - name: Checkout code
+      uses: actions/checkout@v2
+
+    - name: Set up Python
+      uses: actions/setup-python@v2
+      with:
+        python-version: 3.8  # You can choose your desired Python version
+
+    - name: Install dependencies
+      run: pip install -r requirements.txt  # If you have any requirements
+
+    - name: Run Python script
+      run: python matrix.py  # Replace with the actual filename of your script
+
+    - name: Display result
+      run: |
+        zeros_count=$(python -c "from your_script import count_zeros; matrix = [[1, 0, 2], [0, 3, 4], [5, 0, 6]]; print(count_zeros(matrix))")
+        echo "Number of zeros in the matrix: $zeros_count"
